e268f3682497109d9725c0e4feaa7b3997abf67f
Daniel De La Pena <del17011@byui.edu>
2023-05-02 23:51:48 -0600

    starting with job application controllers

--- JobSeaAPI/Controllers/JobApplicationController.cs
+++ JobSeaAPI/Controllers/JobApplicationController.cs
@@ -1,6 +1,47 @@
-﻿namespace JobSeaAPI.Controllers
+﻿using AutoMapper;
+using JobSeaAPI.Services;
+using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobSeaAPI.Controllers
 {
-    public class JobApplicationController
+    [ApiController]
+    [Route("jobSea/[Controller]")]
+    public class JobApplicationController:ControllerBase
     {
+        private readonly IMapper _mapper;
+        private readonly ILoggerCustom _logger;
+        private readonly IConfiguration _configuration;
+        private readonly ITokenService _tokenService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        protected APIResponse _response;
+        
+        public JobApplicationController(IMapper mapper, ILoggerCustom logger, IHttpContextAccessor httpContextAccessor, IConfiguration configuration, ITokenService tokenService)
+        {
+            _mapper = mapper;
+            _logger = logger;
+            _configuration = configuration;
+            _tokenService = tokenService;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        // Gets Applications for a specific user, based on specific criteria
+        [HttpGet("GetAllApplications")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult<APIResponse>> GetApplications()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
     }
 }
