790e8a7cc382ebfd2a56add391ea7755a7da2574
Daniel De La Pena <del17011@byui.edu>
2023-08-06 21:47:32 -0600

    started update controller so i know its the next thing to work on

---
+++ JobSeaAPI/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobSeaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
