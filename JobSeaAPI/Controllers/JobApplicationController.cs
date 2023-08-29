559784d1bc360ee74dc6e15bab8e0e98d1a823e6
Daniel De La Pena <del17011@byui.edu>
2023-08-28 20:26:22 -0600

    moved status to its own endpoint

--- JobSeaAPI/Controllers/JobApplicationController.cs
+++ JobSeaAPI/Controllers/JobApplicationController.cs
@@ -86,6 +86,7 @@ namespace JobSeaAPI.Controllers
                 if (actionResult is not null) return actionResult;
 
                 Application application = _applicationsRepo.GetApplication(applicationId, userId);
+                ApplicationDTO applicationDTO = _mapper.Map<ApplicationDTO>(application);
                 _response.Result = application;
                 _response.Errors = null;
                 _response.StatusCode = System.Net.HttpStatusCode.OK;
@@ -183,31 +184,6 @@ namespace JobSeaAPI.Controllers
 
                 await _applicationsRepo.DeleteApplication(applicationId, userId);
                 return NoContent();
-            }
-            catch (JobSeaException ex)
-            {
-                return _exceptionHandler.returnExceptionResponse(ex, _response);
-            }
-            catch (Exception ex)
-            {
-                return _exceptionHandler.returnExceptionResponse(ex, _response);
-            }
-        }
-
-        [HttpGet("statusOptions")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<APIResponse>> GetStatusOptions()
-        {
-            try
-            {
-                List<Status> statuses = _statusRepository.GetStatuses();
-                _response.IsSuccess = true;
-                _response.StatusCode = System.Net.HttpStatusCode.OK;
-                _response.Errors = null;
-                _response.Result = statuses;
-                return Ok(_response);
             }
             catch (JobSeaException ex)
             {
---
+++ JobSeaAPI/Controllers/StatusController.cs
@@ -0,0 +1,47 @@
+﻿using JobSeaAPI.Exceptions;
+using JobSeaAPI.Models;
+using JobSeaAPI.Repository.IRepository;
+using JobSeaAPI.Services;
+using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobSeaAPI.Controllers
+{
+    public class StatusController:ControllerBase
+    {
+        private readonly IStatusRepository _statusRepository;
+        protected APIResponse _response;
+        private readonly IExceptionHandler _exceptionHandler;
+        public StatusController( IStatusRepository statusRepository, IExceptionHandler exceptionHandler)
+        {
+            _response = new();
+            _statusRepository = statusRepository;
+            _exceptionHandler = exceptionHandler;
+        }
+
+        [HttpGet("statusOptions")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> GetStatusOptions()
+        {
+            try
+            {
+                List<Status> statuses = _statusRepository.GetStatuses();
+                _response.IsSuccess = true;
+                _response.StatusCode = System.Net.HttpStatusCode.OK;
+                _response.Errors = null;
+                _response.Result = statuses;
+                return Ok(_response);
+            }
+            catch (JobSeaException ex)
+            {
+                return _exceptionHandler.returnExceptionResponse(ex, _response);
+            }
+            catch (Exception ex)
+            {
+                return _exceptionHandler.returnExceptionResponse(ex, _response);
+            }
+        }
+    }
+}
