a4ce05e6e4ff99c551b00da8fc5099d303c8d10d
Daniel De La Pena <del17011@byui.edu>
2024-07-07 20:59:35 -0600

    fix salary nullable issue

---
+++ JobSeaAPI/Migrations/20240708025742_MakeSalaryNullableAgain.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobSeaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeSalaryNullableAgain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Salary",
+                table: "Applications",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Salary",
+                table: "Applications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
--- JobSeaAPI/Models/Application.cs
+++ JobSeaAPI/Models/Application.cs
@@ -13,7 +13,7 @@ namespace JobSeaAPI.Models
         public string Company { get; set; } 
         [Required]
         public string JobTitle { get; set; } 
-        public int Salary { get; set; } 
+        public int? Salary { get; set; } 
         public string? City { get; set; }
         public string? State { get; set; }
         public string? Link { get; set; } 
